239a8daa95d6c22717d59611facea7984b5e3331
Jarrod Servilla <jarrod.servilla@gmail.com>
2021-05-06 15:48:49 -0400

    Import base

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>Hello, thanks for using my template.</div>
+  );
+}
